66ea2ff7191287d4adf60c86a21b9b4c4abdd335
Kim Song <kimsong432@gmail.com>
2022-06-27 16:50:51 +0700

    group service

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -3,3 +3,34 @@ import { dbClient } from "../libs";
 export const findAllGroups = async () => {
   return await dbClient.group.findMany();
 };
+
+export const findOneGroup = async (id: number) => {
+  const group = await dbClient.group.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!group) throw new Error(`Group ${id} not found.`);
+  return group;
+};
+
+export const createGroup = async (id: number, name: string) => {
+  return await dbClient.group.upsert({
+    where: {
+      id,
+    },
+    create: {
+      id,
+      name,
+    },
+    update: {
+      id,
+      name,
+    },
+  });
+};
+
+export const deleteGroup = async (id: number) => {
+  await dbClient.group.delete({ where: { id } });
+};
