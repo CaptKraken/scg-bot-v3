0f0f332260273ec5d2fa55bcfcab48add9feb5e5
Kim Song <kimsong432@gmail.com>
2022-06-28 14:38:26 +0700

    added quote schema and migration

---
+++ prisma/migrations/20220628073741_create_quotes_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "quotes" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "quotes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "quotes_text_key" ON "quotes"("text");
