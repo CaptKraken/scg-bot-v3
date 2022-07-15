eacae2e1ec309c172bd0b466b61fb5fa53890c1a
Kim Song <kimsong432@gmail.com>
2022-07-15 13:20:53 +0700

    seeing jobs

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -88,5 +88,5 @@ export const createCronJobs = async () => {
     await generateJob(dc.schedule, dc.id, dc.groupId);
   });
   all = [];
-  console.log(`[INFO]: ${scheduler.scheduledJobs.length} jobs created.`);
+  console.log(`[INFO]: ${scheduler.scheduledJobs} jobs created.`);
 };
